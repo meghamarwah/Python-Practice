26d28bfd7d08f57cceba6b0e28c14ea2502a6e1f
meghamarwah <megha.marwah1@gmail.com>
2020-07-20 19:37:12 +0530

    20th jul changes

--- pytest_practice/testfixture.py
+++ pytest_practice/testfixture.py
@@ -1,11 +1,18 @@
+from unittest.runner import TextTestRunner
+from HtmlTestRunner import runner
 import pytest
-
+import pytest_html
+from selenium import webdriver
 @pytest.fixture()
 def megha():
     print('This is megha block!')
+    driver = webdriver.Firefox(executable_path='C:/Users/Megha/Documents/Python/Selproject/geckodriver_v25/geckodriver.exe')
+    driver.get('https://www.flipkart.com')
 
 def testmethod1(megha):
     print('this is test method1')
 
 def testmethod2(megha):
     print('this is test method2')
+
+pytest.main('')
